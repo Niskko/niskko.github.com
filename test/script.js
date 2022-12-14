7dde4c65c5bfa2062b4fc5e9aa389791b43662de
Niskko <niskko.yt@gmail.com>
2022-12-14 22:47:06 +0100

    Modification selection nav

--- test/script.js
+++ test/script.js
@@ -1,12 +1,14 @@
 var carousel = document.querySelector('.carousel');
 var figure = document.querySelector('figure');
-var nave = document.querySelectorAll('nav')[1];
+var slide = document.querySelectorAll('.slide');
 document.querySelectorAll('img');
 var numImages = document.querySelectorAll('img').length;
 var theta = (2 * Math.PI) / numImages;
 var currImage = 0;
-nave.addEventListener('click', onClick);
+slide[0].addEventListener('click', onClick);
+slide[1].addEventListener('click', onClick);
 function onClick(e) {
+	console.log('test');
 	e.stopPropagation();
 	var t = e.target;
 	if (t.tagName.toUpperCase() != 'BUTTON') return;
